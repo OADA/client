30d69b81805a8d8468ba3f8b1a69f4fcf6d15f84
Aaron Ault <aultac@gmail.com>
2020-07-04 18:48:36 -0400

    v1.0.4, added better debug message for paths not in tree, also added 403 test for tree put on /resources

--- lib/utils.ts
+++ lib/utils.ts
@@ -22,7 +22,7 @@ export function getObjectAtPath(tree: object, path: Array<string>): object {
     } else if (acc["*"]) {
       return acc["*"];
     } else {
-      throw new Error("Specified path does not exist in the tree.");
+      throw new Error("Specified path /"+path.join('/')+" does not exist in the tree.");
     }
   }, tree);
 }
@@ -37,7 +37,7 @@ export function toTreePath(tree: object, path: Array<string>): Array<string> {
       treePath.push("*");
       return acc["*"];
     } else {
-      throw new Error("Specified path does not exist in the tree.");
+      throw new Error("Specified path /"+path.join('/')+" does not exist in the tree.");
     }
   }, tree);
   return treePath;
