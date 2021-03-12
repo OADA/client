150d0e170a303749f144ec5213c251002f25fb22
Alex Layton <alex@layton.in>
2021-03-12 15:32:48 -0500

    Make http2 honor `NODE_TLS_REJECT_UNAUTHORIZED`

--- lib/fetch.ts
+++ lib/fetch.ts
@@ -1,4 +1,11 @@
-import { fetch } from "fetch-h2";
+import { context } from "fetch-h2";
+
+// Create our own context to honor NODE_TLS_REJECT_UNAUTHORIZED like https
+const { fetch } = context({
+  session: {
+    rejectUnauthorized: process.env.NODE_TLS_REJECT_UNAUTHORIZED !== "0",
+  },
+});
 
 // cross-fetch has fetch as default export
 export default fetch;
